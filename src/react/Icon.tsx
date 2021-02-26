cedf07480ea6779a60cfb70ecef50ccbb8fdcfc2
Martin Munilla <martinmunillas@gmail.com>
2021-02-26 15:33:12 -0300

    feature/WTW-425 added default size

--- src/react/Icon.tsx
+++ src/react/Icon.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { IconComponent } from './types';
 
-const Icon: IconComponent = ({ children, ...props }) => {
-  return <svg {...props}>{children}</svg>;
+const Icon: IconComponent = ({ children, width, height, ...props }) => {
+    return (
+        <svg {...props} width={width || '100px'} height={height || '100px'}>
+            {children}
+        </svg>
+    );
 };
 
 export default Icon;
