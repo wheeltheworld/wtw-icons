b281fb010686698a2e11b45b5ebce48c11384f30
martinmunilla <martinmunillas@gmail.com>
2021-03-01 13:40:01 -0300

    bugfix/second try

--- src/react/Icon.tsx
+++ src/react/Icon.tsx
@@ -3,7 +3,7 @@ import { IconComponent } from './types';
 
 const Icon: IconComponent = ({ children, width, height, ...props }) => {
     return (
-        <svg {...props} width={width || '100px'} height={height || '100px'}>
+        <svg {...props} width={width || '100px'} height={height || '100px'} style={{maxHeight: '100%', maxWidth: '100%'}} >
             {children}
         </svg>
     );
