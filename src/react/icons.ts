8a24c695958c881f5cdd14f4f4f7f1e064352ae5
Gonzalo Henríquez <gonzalo@gowheeltheworld.com>
2021-03-05 03:45:31 -0300

    WTW-531 adds extra signup icons

--- src/react/icons.ts
+++ src/react/icons.ts
@@ -1,44 +1,66 @@
+import AccessibleBathroom from  './icons/AccessibleBathroom';
+import AccessibleEquipment from  './icons/AccessibleEquipment';
+import AccessibleTransportation from  './icons/AccessibleTransportation';
+import Activities from  './icons/Activities';
+import Assistance from  './icons/Assistance';
 import Atm from  './icons/Atm';
 import Auditorium from  './icons/Auditorium';
+import AuditoryGuidance from  './icons/AuditoryGuidance';
 import Backpacker from  './icons/Backpacker';
 import Bar from  './icons/Bar';
 import Bathroom from  './icons/Bathroom';
 import BathroomAccessInterior from  './icons/BathroomAccessInterior';
+import BathroomInterior from  './icons/BathroomInterior';
+import BathroomSink from  './icons/BathroomSink';
 import BeachAccess from  './icons/BeachAccess';
 import BeachFront from  './icons/BeachFront';
 import Bed from  './icons/Bed';
 import BedBreakfast from  './icons/BedBreakfast';
 import BedroomAccess from  './icons/BedroomAccess';
 import BeerBar from  './icons/BeerBar';
+import Blind from  './icons/Blind';
 import Boat from  './icons/Boat';
+import Braille from  './icons/Braille';
 import Breakfast from  './icons/Breakfast';
 import BuildingEntrance from  './icons/BuildingEntrance';
+import Bus from  './icons/Bus';
 import Cabin from  './icons/Cabin';
 import Cafeteria from  './icons/Cafeteria';
+import Car from  './icons/Car';
 import ChangingRoom from  './icons/ChangingRoom';
 import Closet from  './icons/Closet';
 import CommonBathroom from  './icons/CommonBathroom';
 import Complete from  './icons/Complete';
+import Crutches from  './icons/Crutches';
 import CulturalPlace from  './icons/CulturalPlace';
 import Culture from  './icons/Culture';
 import Daycare from  './icons/Daycare';
+import Deaf from  './icons/Deaf';
 import Dinner from  './icons/Dinner';
 import Done from  './icons/Done';
+import ElectricDeviceWheelchair from  './icons/ElectricDeviceWheelchair';
 import Entertainment from  './icons/Entertainment';
 import Entrance from  './icons/Entrance';
 import Equipment from  './icons/Equipment';
+import FoldablePowerWheelchair from  './icons/FoldablePowerWheelchair';
 import FoodService from  './icons/FoodService';
 import GeneralAttribute from  './icons/GeneralAttribute';
 import Guides from  './icons/Guides';
 import Gym from  './icons/Gym';
+import HearingAid from  './icons/HearingAid';
 import HeatedPool from  './icons/HeatedPool';
+import HeavyPowerWheelchair from  './icons/HeavyPowerWheelchair';
+import HighBathtub from  './icons/HighBathtub';
 import Hotel from  './icons/Hotel';
 import Interior from  './icons/Interior';
+import JeepCar from  './icons/JeepCar';
 import Key from  './icons/Key';
 import Keys from  './icons/Keys';
 import LakeSea from  './icons/LakeSea';
 import LandActivity from  './icons/LandActivity';
 import Lobby from  './icons/Lobby';
+import LowerBathtub from  './icons/LowerBathtub';
+import LowerSteps from  './icons/LowerSteps';
 import MeasuringTape from  './icons/MeasuringTape';
 import MeetingPoint from  './icons/MeetingPoint';
 import Minus from  './icons/Minus';
@@ -47,86 +69,128 @@ import MobilePhone from  './icons/MobilePhone';
 import Motel from  './icons/Motel';
 import MultiDayTrip from  './icons/MultiDayTrip';
 import Next from  './icons/Next';
+import No from  './icons/No';
 import Notification from  './icons/Notification';
 import NotificationActive from  './icons/NotificationActive';
 import Nurse from  './icons/Nurse';
+import OneStep from  './icons/OneStep';
 import Other from  './icons/Other';
 import Outdoor from  './icons/Outdoor';
 import Park from  './icons/Park';
 import Parking from  './icons/Parking';
 import Pending from  './icons/Pending';
+import PetFriendly from  './icons/PetFriendly';
 import Places from  './icons/Places';
 import PlacesToStay from  './icons/PlacesToStay';
 import Playground from  './icons/Playground';
 import Plus from  './icons/Plus';
 import Profile from  './icons/Profile';
+import QueenBed from  './icons/QueenBed';
 import Reception from  './icons/Reception';
 import Resort from  './icons/Resort';
 import Restaurant from  './icons/Restaurant';
 import Restrictions from  './icons/Restrictions';
+import RollInShower from  './icons/RollInShower';
+import RoomEntrance from  './icons/RoomEntrance';
 import Sauna from  './icons/Sauna';
+import Scooter from  './icons/Scooter';
+import SedanCar from  './icons/SedanCar';
 import Shopping from  './icons/Shopping';
 import Shower from  './icons/Shower';
+import ShowerGrabBar from  './icons/ShowerGrabBar';
+import ShowerLowStep from  './icons/ShowerLowStep';
+import ShowerSeat from  './icons/ShowerSeat';
 import Sink from  './icons/Sink';
+import SinkTap from  './icons/SinkTap';
 import SnackBar from  './icons/SnackBar';
 import Spa from  './icons/Spa';
+import Stairs from  './icons/Stairs';
+import StepFree from  './icons/StepFree';
 import SwimmingPool from  './icons/SwimmingPool';
 import TeaRoom from  './icons/TeaRoom';
 import Terrace from  './icons/Terrace';
 import ThingsToDo from  './icons/ThingsToDo';
+import ThreeSteps from  './icons/ThreeSteps';
 import Toilet from  './icons/Toilet';
+import ToiletGrabBar from  './icons/ToiletGrabBar';
 import ToursActivities from  './icons/ToursActivities';
 import Transport from  './icons/Transport';
 import Transportation from  './icons/Transportation';
+import TurningRadius from  './icons/TurningRadius';
 import WTWIso from  './icons/WTWIso';
 import WTWLogo from  './icons/WTWLogo';
 import WTWLogoVertical from  './icons/WTWLogoVertical';
+import Walker from  './icons/Walker';
+import WalkingAid from  './icons/WalkingAid';
 import WaterActivity from  './icons/WaterActivity';
+import WheelchairRamp from  './icons/WheelchairRamp';
 import Wifi from  './icons/Wifi';
 import Work from  './icons/Work';
 
 const icons = {
+	accessiblebathroom: AccessibleBathroom,
+	accessibleequipment: AccessibleEquipment,
+	accessibletransportation: AccessibleTransportation,
+	activities: Activities,
+	assistance: Assistance,
 	atm: Atm,
 	auditorium: Auditorium,
+	auditoryguidance: AuditoryGuidance,
 	backpacker: Backpacker,
 	bar: Bar,
 	bathroom: Bathroom,
 	bathroomaccessinterior: BathroomAccessInterior,
+	bathroominterior: BathroomInterior,
+	bathroomsink: BathroomSink,
 	beachaccess: BeachAccess,
 	beachfront: BeachFront,
 	bed: Bed,
 	bedbreakfast: BedBreakfast,
 	bedroomaccess: BedroomAccess,
 	beerbar: BeerBar,
+	blind: Blind,
 	boat: Boat,
+	braille: Braille,
 	breakfast: Breakfast,
 	buildingentrance: BuildingEntrance,
+	bus: Bus,
 	cabin: Cabin,
 	cafeteria: Cafeteria,
+	car: Car,
 	changingroom: ChangingRoom,
 	closet: Closet,
 	commonbathroom: CommonBathroom,
 	complete: Complete,
+	crutches: Crutches,
 	culturalplace: CulturalPlace,
 	culture: Culture,
 	daycare: Daycare,
+	deaf: Deaf,
 	dinner: Dinner,
 	done: Done,
+	electricdevicewheelchair: ElectricDeviceWheelchair,
 	entertainment: Entertainment,
 	entrance: Entrance,
 	equipment: Equipment,
+	foldablepowerwheelchair: FoldablePowerWheelchair,
 	foodservice: FoodService,
 	generalattribute: GeneralAttribute,
 	guides: Guides,
 	gym: Gym,
+	hearingaid: HearingAid,
 	heatedpool: HeatedPool,
+	heavypowerwheelchair: HeavyPowerWheelchair,
+	highbathtub: HighBathtub,
 	hotel: Hotel,
 	interior: Interior,
+	jeepcar: JeepCar,
 	key: Key,
 	keys: Keys,
 	lakesea: LakeSea,
 	landactivity: LandActivity,
 	lobby: Lobby,
+	lowerbathtub: LowerBathtub,
+	lowersteps: LowerSteps,
 	measuringtape: MeasuringTape,
 	meetingpoint: MeetingPoint,
 	minus: Minus,
@@ -135,87 +199,129 @@ const icons = {
 	motel: Motel,
 	multidaytrip: MultiDayTrip,
 	next: Next,
+	no: No,
 	notification: Notification,
 	notificationactive: NotificationActive,
 	nurse: Nurse,
+	onestep: OneStep,
 	other: Other,
 	outdoor: Outdoor,
 	park: Park,
 	parking: Parking,
 	pending: Pending,
+	petfriendly: PetFriendly,
 	places: Places,
 	placestostay: PlacesToStay,
 	playground: Playground,
 	plus: Plus,
 	profile: Profile,
+	queenbed: QueenBed,
 	reception: Reception,
 	resort: Resort,
 	restaurant: Restaurant,
 	restrictions: Restrictions,
+	rollinshower: RollInShower,
+	roomentrance: RoomEntrance,
 	sauna: Sauna,
+	scooter: Scooter,
+	sedancar: SedanCar,
 	shopping: Shopping,
 	shower: Shower,
+	showergrabbar: ShowerGrabBar,
+	showerlowstep: ShowerLowStep,
+	showerseat: ShowerSeat,
 	sink: Sink,
+	sinktap: SinkTap,
 	snackbar: SnackBar,
 	spa: Spa,
+	stairs: Stairs,
+	stepfree: StepFree,
 	swimmingpool: SwimmingPool,
 	tearoom: TeaRoom,
 	terrace: Terrace,
 	thingstodo: ThingsToDo,
+	threesteps: ThreeSteps,
 	toilet: Toilet,
+	toiletgrabbar: ToiletGrabBar,
 	toursactivities: ToursActivities,
 	transport: Transport,
 	transportation: Transportation,
+	turningradius: TurningRadius,
 	wtwiso: WTWIso,
 	wtwlogo: WTWLogo,
 	wtwlogovertical: WTWLogoVertical,
+	walker: Walker,
+	walkingaid: WalkingAid,
 	wateractivity: WaterActivity,
+	wheelchairramp: WheelchairRamp,
 	wifi: Wifi,
 	work: Work,
 };
 
 const IconList = [
+	'accessiblebathroom',
+	'accessibleequipment',
+	'accessibletransportation',
+	'activities',
+	'assistance',
 	'atm',
 	'auditorium',
+	'auditoryguidance',
 	'backpacker',
 	'bar',
 	'bathroom',
 	'bathroomaccessinterior',
+	'bathroominterior',
+	'bathroomsink',
 	'beachaccess',
 	'beachfront',
 	'bed',
 	'bedbreakfast',
 	'bedroomaccess',
 	'beerbar',
+	'blind',
 	'boat',
+	'braille',
 	'breakfast',
 	'buildingentrance',
+	'bus',
 	'cabin',
 	'cafeteria',
+	'car',
 	'changingroom',
 	'closet',
 	'commonbathroom',
 	'complete',
+	'crutches',
 	'culturalplace',
 	'culture',
 	'daycare',
+	'deaf',
 	'dinner',
 	'done',
+	'electricdevicewheelchair',
 	'entertainment',
 	'entrance',
 	'equipment',
+	'foldablepowerwheelchair',
 	'foodservice',
 	'generalattribute',
 	'guides',
 	'gym',
+	'hearingaid',
 	'heatedpool',
+	'heavypowerwheelchair',
+	'highbathtub',
 	'hotel',
 	'interior',
+	'jeepcar',
 	'key',
 	'keys',
 	'lakesea',
 	'landactivity',
 	'lobby',
+	'lowerbathtub',
+	'lowersteps',
 	'measuringtape',
 	'meetingpoint',
 	'minus',
@@ -224,41 +330,61 @@ const IconList = [
 	'motel',
 	'multidaytrip',
 	'next',
+	'no',
 	'notification',
 	'notificationactive',
 	'nurse',
+	'onestep',
 	'other',
 	'outdoor',
 	'park',
 	'parking',
 	'pending',
+	'petfriendly',
 	'places',
 	'placestostay',
 	'playground',
 	'plus',
 	'profile',
+	'queenbed',
 	'reception',
 	'resort',
 	'restaurant',
 	'restrictions',
+	'rollinshower',
+	'roomentrance',
 	'sauna',
+	'scooter',
+	'sedancar',
 	'shopping',
 	'shower',
+	'showergrabbar',
+	'showerlowstep',
+	'showerseat',
 	'sink',
+	'sinktap',
 	'snackbar',
 	'spa',
+	'stairs',
+	'stepfree',
 	'swimmingpool',
 	'tearoom',
 	'terrace',
 	'thingstodo',
+	'threesteps',
 	'toilet',
+	'toiletgrabbar',
 	'toursactivities',
 	'transport',
 	'transportation',
+	'turningradius',
 	'wtwiso',
 	'wtwlogo',
 	'wtwlogovertical',
+	'walker',
+	'walkingaid',
 	'wateractivity',
+	'wheelchairramp',
 	'wifi',
 	'work',
 ]
---
+++ src/react/icons/BathroomInterior.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import Icon from '../Icon';
+import { IconComponent } from '../types';
+
+const BathroomInterior: IconComponent = (props) => (
+    <Icon viewBox="0 0 24 24" {...props}>
+        <g fill="currentColor">
+            <path
+                d="M9.13 18.97H3.85V4.36h16.28v14.61h-5.27"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeMiterlimit="10"
+            />
+            <path
+                d="M1 3.2l2.85 1.16"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeMiterlimit="10"
+            />
+            <path
+                d="M23 3.2l-2.85 1.16"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeMiterlimit="10"
+            />
+            <path
+                d="M1 20.8l2.85-1.83"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeMiterlimit="10"
+            />
+            <path
+                d="M22.98 20.8l-2.85-1.83"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeMiterlimit="10"
+            />
+            <g transform="translate(.5 .5)">
+                <path
+                    d="M8.62 15.04v-4.3c0-.53.43-.96.96-.96h3.83c.53 0 .96.43.96.96v4.3"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeMiterlimit="10"
+                />
+                <path
+                    d="M15.79 15.04H7.18V16c0 1.58 1.29 2.87 2.87 2.87v1.43h2.87v-1.43c1.58 0 2.87-1.29 2.87-2.87v-.96z"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeMiterlimit="10"
+                />
+            </g>
+        </g>
+    </Icon>
+);
+
+export default BathroomInterior;
