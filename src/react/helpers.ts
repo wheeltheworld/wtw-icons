444c6c83811ec39511b9aa7a1f8e4452d01699f6
Felipe Rueda <fe.rueda@outlook.com>
2021-03-29 21:07:15 -0300

    Add edit icon (#20)
    * Add new Edit
    * 1.10.0
    * Remove Browse, CheckList and SignUp

--- src/react/helpers.ts
+++ src/react/helpers.ts
@@ -32,7 +32,6 @@ import Blind from  './_icons/Blind';
 import Boat from  './_icons/Boat';
 import Braille from  './_icons/Braille';
 import Breakfast from  './_icons/Breakfast';
-import Browse from  './_icons/Browse';
 import BuildingEntrance from  './_icons/BuildingEntrance';
 import Bus from  './_icons/Bus';
 import BusinessCenter from  './_icons/BusinessCenter';
@@ -43,7 +42,6 @@ import Car from  './_icons/Car';
 import Chair from  './_icons/Chair';
 import Champagne from  './_icons/Champagne';
 import ChangingRoom from  './_icons/ChangingRoom';
-import CheckList from  './_icons/CheckList';
 import Clock from  './_icons/Clock';
 import Closet from  './_icons/Closet';
 import CoffeeMaker from  './_icons/CoffeeMaker';
@@ -60,6 +58,7 @@ import Dinner from  './_icons/Dinner';
 import Documentation from  './_icons/Documentation';
 import Done from  './_icons/Done';
 import DryCleaning from  './_icons/DryCleaning';
+import Edit from  './_icons/Edit';
 import ElectricDeviceWheelchair from  './_icons/ElectricDeviceWheelchair';
 import Electricity from  './_icons/Electricity';
 import Elevator from  './_icons/Elevator';
@@ -163,7 +162,6 @@ import Shower from  './_icons/Shower';
 import ShowerGrabBar from  './_icons/ShowerGrabBar';
 import ShowerLowStep from  './_icons/ShowerLowStep';
 import ShowerSeat from  './_icons/ShowerSeat';
-import SignUp from  './_icons/SignUp';
 import Sink from  './_icons/Sink';
 import SinkTap from  './_icons/SinkTap';
 import SmokeFree from  './_icons/SmokeFree';
@@ -248,7 +246,6 @@ export const iconsArray = [
 	'boat',
 	'braille',
 	'breakfast',
-	'browse',
 	'buildingentrance',
 	'bus',
 	'businesscenter',
@@ -259,7 +256,6 @@ export const iconsArray = [
 	'chair',
 	'champagne',
 	'changingroom',
-	'checklist',
 	'clock',
 	'closet',
 	'coffeemaker',
@@ -276,6 +272,7 @@ export const iconsArray = [
 	'documentation',
 	'done',
 	'drycleaning',
+	'edit',
 	'electricdevicewheelchair',
 	'electricity',
 	'elevator',
@@ -379,7 +376,6 @@ export const iconsArray = [
 	'showergrabbar',
 	'showerlowstep',
 	'showerseat',
-	'signup',
 	'sink',
 	'sinktap',
 	'smokefree',
@@ -463,7 +459,6 @@ export const iconsObject = {
 	boat: Boat,
 	braille: Braille,
 	breakfast: Breakfast,
-	browse: Browse,
 	buildingentrance: BuildingEntrance,
 	bus: Bus,
 	businesscenter: BusinessCenter,
@@ -474,7 +469,6 @@ export const iconsObject = {
 	chair: Chair,
 	champagne: Champagne,
 	changingroom: ChangingRoom,
-	checklist: CheckList,
 	clock: Clock,
 	closet: Closet,
 	coffeemaker: CoffeeMaker,
@@ -491,6 +485,7 @@ export const iconsObject = {
 	documentation: Documentation,
 	done: Done,
 	drycleaning: DryCleaning,
+	edit: Edit,
 	electricdevicewheelchair: ElectricDeviceWheelchair,
 	electricity: Electricity,
 	elevator: Elevator,
@@ -594,7 +589,6 @@ export const iconsObject = {
 	showergrabbar: ShowerGrabBar,
 	showerlowstep: ShowerLowStep,
 	showerseat: ShowerSeat,
-	signup: SignUp,
 	sink: Sink,
 	sinktap: SinkTap,
 	smokefree: SmokeFree,
