52769e15acc28eb424ae622f3499e3d994e8a9d5
Harold <harold@gowheeltheworld.com>
2021-09-13 11:38:57 -0400

    WTW-1059 add WideEntranceShower icon

---
+++ src/react/_icons/WideEntranceShower.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import Icon from '../components/Icon';
+import { IconComponent } from '../types';
+
+const WideEntranceShower: IconComponent = (props) => (
+    <Icon viewBox="0 0 40 40" {...props}>
+        <g fill="none">
+            <path
+                d="M3.16663 38V2H37V37.8333"
+                stroke="currentColor"
+                stroke-width="1.5"
+                stroke-miterlimit="10"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+            <path
+                d="M8.83325 16.1666H18.3333C18.3333 13.5 16.1666 11.5 13.6666 11.5C10.9999 11.3333 8.83325 13.5 8.83325 16.1666Z"
+                stroke="currentColor"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+            <path
+                d="M16.9999 20.4999V19.3333"
+                stroke="currentColor"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+            <path
+                d="M13.6666 22.6667V21.5"
+                stroke="currentColor"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+            <path
+                d="M10.1666 20.3334V19.1667"
+                stroke="currentColor"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+            <path
+                d="M13.6666 11.4998V7.6665"
+                stroke="currentColor"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+            <path
+                d="M3.16663 7.6665H13.6666"
+                stroke="currentColor"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+        </g>
+    </Icon>
+);
+
+export default WideEntranceShower;
