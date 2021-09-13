1ead008fc0203c9aa179c48670321ea1c1b043f3
Harold <harold@gowheeltheworld.com>
2021-09-13 10:34:07 -0400

    WTW-1059 update LocationBlack icon

--- src/react/_icons/LocationBlack.tsx
+++ src/react/_icons/LocationBlack.tsx
@@ -5,7 +5,7 @@ import { IconComponent } from '../types';
 const LocationBlack: IconComponent = (props) => (
     <Icon viewBox="0 0 24 24" {...props}>
         <path
-            fill="#232323"
+            fill="currentColor"
             d="M12.5,1.5c-3.9,0-8,2.9-8,8c0,4.9,8,13,8,13s8-8.1,8-13C20.5,4.4,16.4,1.5,12.5,1.5z M12.5,12.5 c-1.7,0-3-1.3-3-3s1.3-3,3-3s3,1.3,3,3S14.2,12.5,12.5,12.5z"
         />
     </Icon>
