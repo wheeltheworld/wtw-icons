0bf1268da2b01e470baa9ad3c2c38997ed38a37c
XOD <61284283+iamXOD@users.noreply.github.com>
2021-09-20 08:10:47 -0400

    hotfix/icons color (#39)
    * hotfix missing indirect value for color attributes
    
    The currentColor value of stroke and fill attributes was added as a default
    value to these icons:
    - Accessibility
    - Happy
    - Money
    
    * 1.28.1
    
    * Make currentColor default value for fill and stroke
    
    Co-authored-by: Harold <harold@gowheeltheworld.com>

--- src/react/_icons/Money.tsx
+++ src/react/_icons/Money.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Icon from '../components/Icon';
 import { IconComponent } from '../types';
 
-const Money: IconComponent = (props) => (
+const Money: IconComponent = ({ fill = 'currentColor', stroke = 'currentColor', ...props }) => (
     <Icon viewBox="0 0 24 24" {...props}>
         <g
             strokeLinecap="round"
@@ -10,9 +10,9 @@ const Money: IconComponent = (props) => (
             strokeWidth={1.5}
             transform="translate(0.5 0.5)"
             fill="currentColor"
-            stroke={props.stroke}
+            stroke={stroke}
         >
-            <circle fill="none" stroke={props.stroke} strokeMiterlimit={10} cx={12} cy={12} r={11} />
+            <circle fill="none" stroke={stroke} strokeMiterlimit={10} cx={12} cy={12} r={11} />
             <line data-color="color-2" fill="none" strokeMiterlimit={10} x1={12} y1={5} x2={12} y2={19} />
             <path
                 data-color="color-2"
