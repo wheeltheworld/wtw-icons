c5d4697b612ad63b29719df30bb601aeb498c434
andre <andrea@gowheeltheworld.com>
2021-09-29 14:42:32 -0300

    updated measuring tape icon

--- src/react/_icons/MeasuringTape.tsx
+++ src/react/_icons/MeasuringTape.tsx
@@ -3,21 +3,32 @@ import Icon from '../components/Icon';
 import { IconComponent } from '../types';
 
 const MeasuringTape: IconComponent = (props) => (
-    <Icon viewBox="0 0 24 24" {...props}>
-        <g
-            className="prefix__nc-icon-wrapper"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={1.5}
-            transform="translate(.5 .5)"
-            fill="none"
-            stroke="currentColor"
-            strokeMiterlimit={10}
-        >
-            <path data-color="color-2" d="M18 20v-3M13 20v-3M8 20v-3" />
-            <path d="M6 20c-2.761 0-5-1.343-5-3V7c0 1.657 2.239 3 5 3h17v10z" />
-            <ellipse cx={6} cy={7} rx={5} ry={3} data-cap="butt" />
-        </g>
+    <Icon viewBox="0 0 40 40" {...props}>
+         <g
+        stroke="currentColor"
+        strokeMiterlimit="10"
+        strokeWidth="1.5"
+        clipPath="url(#clip0)"
+      >
+        <path
+          strokeLinecap="round"
+          d="M39.357 29.445v8.9H4.63c-2.186 0-3.987-1.832-3.987-4.057v-7.853c0-6.545 5.145-11.78 11.576-11.78 6.43 0 11.575 5.366 11.575 11.91l-.128 11.519"
+        ></path>
+        <path d="M23.794 33.11h15.563M12.09 32.325c3.126 0 5.66-2.579 5.66-5.76 0-3.18-2.534-5.759-5.66-5.759-3.125 0-5.66 2.579-5.66 5.76 0 3.18 2.535 5.759 5.66 5.759z"></path>
+        <path
+          strokeLinecap="round"
+          d="M34.984 34.812v-1.44M31.383 34.812v-1.44M27.524 34.812v-1.44"
+        ></path>
+      </g>
+      <defs>
+        <clipPath id="clip0">
+          <path
+            fill="currentColor"
+            d="M0 0H40V25H0z"
+            transform="translate(0 14)"
+          ></path>
+        </clipPath>
+      </defs>
     </Icon>
 );
 
