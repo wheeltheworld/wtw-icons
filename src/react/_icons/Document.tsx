2a4d9f1d9571f27ad9d63dc909b29440332b22e1
martinmunillas <martinmunillas@gmail.com>
2021-11-18 18:13:08 -0300

    fix: invalid props

--- src/react/_icons/Document.tsx
+++ src/react/_icons/Document.tsx
@@ -4,11 +4,41 @@ import { IconComponent } from '../types';
 
 const Document: IconComponent = (props) => (
     <Icon viewBox="0 0 24 24" fill="none" {...props}>
-  <path d="M1.42105 2H14.8158C14.9211 2 15.0526 2.05021 15.1316 2.12552L18.8947 6.01674C18.9737 6.09205 19 6.19247 19 6.29289V25.5983C19 25.8243 18.8158 26 18.5789 26H1.42105C1.18421 26 1 25.8243 1 25.5983V2.40167C1 2.17573 1.18421 2 1.42105 2Z" stroke="currentColor" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" />
-  <path d="M14.3449 2.97467V6.66505C14.3449 6.91609 14.5554 7.11693 14.8449 7.11693L18.3712 7.14203" stroke="currentColor" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" />
-  <path d="M4.18421 11.0126H10.6316" stroke="currentColor" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" />
-  <path d="M4.07959 14.5217H15.6849" stroke="currentColor" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" />
-  <path d="M4.07959 18.5754H15.6849" stroke="currentColor" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" />
+        <path
+            d="M1.42105 2H14.8158C14.9211 2 15.0526 2.05021 15.1316 2.12552L18.8947 6.01674C18.9737 6.09205 19 6.19247 19 6.29289V25.5983C19 25.8243 18.8158 26 18.5789 26H1.42105C1.18421 26 1 25.8243 1 25.5983V2.40167C1 2.17573 1.18421 2 1.42105 2Z"
+            stroke="currentColor"
+            stroke-width="1.5"
+            stroke-miterlimit="10"
+            stroke-linecap="round"
+        />
+        <path
+            d="M14.3449 2.97467V6.66505C14.3449 6.91609 14.5554 7.11693 14.8449 7.11693L18.3712 7.14203"
+            stroke="currentColor"
+            stroke-width="1.5"
+            stroke-miterlimit="10"
+            stroke-linecap="round"
+        />
+        <path
+            d="M4.18421 11.0126H10.6316"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+        />
+        <path
+            d="M4.07959 14.5217H15.6849"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+        />
+        <path
+            d="M4.07959 18.5754H15.6849"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+        />
     </Icon>
 );
 
