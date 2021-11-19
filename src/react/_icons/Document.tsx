3cafdb8c9e67eaa4a9d825720ddc6960879471c6
martinmunillas <martinmunillas@gmail.com>
2021-11-19 09:49:07 -0300

    fix: invalid document props

--- src/react/_icons/Document.tsx
+++ src/react/_icons/Document.tsx
@@ -7,16 +7,16 @@ const Document: IconComponent = (props) => (
         <path
             d="M1.42105 2H14.8158C14.9211 2 15.0526 2.05021 15.1316 2.12552L18.8947 6.01674C18.9737 6.09205 19 6.19247 19 6.29289V25.5983C19 25.8243 18.8158 26 18.5789 26H1.42105C1.18421 26 1 25.8243 1 25.5983V2.40167C1 2.17573 1.18421 2 1.42105 2Z"
             stroke="currentColor"
-            stroke-width="1.5"
-            stroke-miterlimit="10"
-            stroke-linecap="round"
+            strokeWidth="1.5"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
         />
         <path
             d="M14.3449 2.97467V6.66505C14.3449 6.91609 14.5554 7.11693 14.8449 7.11693L18.3712 7.14203"
             stroke="currentColor"
-            stroke-width="1.5"
-            stroke-miterlimit="10"
-            stroke-linecap="round"
+            strokeWidth="1.5"
+            strokeMiterlimit="10"
+            strokeLinecap="round"
         />
         <path
             d="M4.18421 11.0126H10.6316"
