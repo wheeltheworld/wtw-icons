9054356d51e3475a7425a5dd74334c4ffce2fe11
Cristina <riverap.cristina@gmail.com>
2021-12-09 08:11:02 -0500

    Eng21 478/add new icons (#49)
    * [ENG21-478] Add new icons
    
    * [ENG21-478] Add 9 new icons

---
+++ src/react/_icons/Sun.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Icon from '../components/Icon';
+import { IconComponent } from '../types';
+
+const Sun: IconComponent = (props) => (
+    <Icon viewBox="0 0 40 40" {...props}>
+        <g fill="none">
+            <path d="M30.0916 9.87181L33.4923 6.39709" stroke="currentColor" strokeWidth={1.5} strokeMiterlimit={10} strokeLinecap="round"/>
+            <path d="M34.4907 19.9633L39.0005 20.0002" stroke="currentColor"  strokeWidth={1.5} strokeMiterlimit={10} strokeLinecap="round"/>
+            <path d="M33.4923 33.5663L30.0916 30.0916" stroke="currentColor"  strokeWidth={1.5} strokeMiterlimit={10} strokeLinecap="round"/>
+            <path d="M9.87212 9.87181L6.5083 6.39709" stroke="currentColor"  strokeWidth={1.5} strokeMiterlimit={10} strokeLinecap="round"/>
+            <path d="M5.47278 19.9633L1 20.0002" stroke="currentColor" strokeWidth={1.5} strokeMiterlimit={10} strokeLinecap="round"/>
+            <path d="M6.5083 33.5663L9.87212 30.0916" stroke="currentColor" strokeWidth={1.5} strokeMiterlimit={10} strokeLinecap="round"/>
+            <path d="M19.9634 5.47278L20.0003 1" stroke="currentColor" strokeWidth={1.5} strokeMiterlimit={10} strokeLinecap="round"/>
+            <path d="M19.9634 34.4901L20.0003 38.9999" stroke="currentColor" strokeWidth={1.5} strokeMiterlimit={10} strokeLinecap="round"/>
+            <circle cx="20.0002" cy="19.9999" r="9.66934" stroke="currentColor" strokeWidth={1.5}/>
+        </g>
+    </Icon>
+);
+
+export default Sun;
