ca7cbfc36ed959179c84220bf55145b1c2d6a29a
XOD <61284283+iamXOD@users.noreply.github.com>
2022-06-08 10:57:46 -0400

    AMS-97 update open tour icon (#69)
    * update OpenTour icon
    
    * 1.42.2

--- src/react/_icons/OpenTour.tsx
+++ src/react/_icons/OpenTour.tsx
@@ -3,64 +3,43 @@ import Icon from '../components/Icon';
 import { IconComponent } from '../types';
 
 const OpenTour: IconComponent = (props) => (
-    <Icon viewBox="0 0 24 24" {...props}>
-        <g transform="translate(0.5, 0.5)" fill="currentColor">
-            <path
-                data-cap="butt"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={1.5}
-                strokeMiterlimit={10}
-                d="M9.455,18.086l-3.243,1.394 C5.477,19.796,5,20.519,5,21.319V23h14v-1.681c0-0.8-0.477-1.523-1.212-1.838l-3.251-1.388"
-                strokeLinejoin="round"
-                strokeLinecap="round"
-            />
-            <circle
-                data-color="color-2"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={1.5}
-                strokeLinecap="round"
-                strokeMiterlimit={10}
-                cx={12}
-                cy={3}
-                r={2}
-                strokeLinejoin="round"
-            />
-            <circle
-                data-color="color-2"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={1.5}
-                strokeLinecap="round"
-                strokeMiterlimit={10}
-                cx={21}
-                cy={8}
-                r={2}
-                strokeLinejoin="round"
-            />
-            <circle
-                data-color="color-2"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={1.5}
-                strokeLinecap="round"
-                strokeMiterlimit={10}
-                cx={3}
-                cy={8}
-                r={2}
-                strokeLinejoin="round"
-            />
-            <path
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={1.5}
-                strokeLinecap="round"
-                strokeMiterlimit={10}
-                d="M12,19L12,19 c-2.209,0-4-1.791-4-4v-1c0-2.209,1.791-4,4-4h0c2.209,0,4,1.791,4,4v1C16,17.209,14.209,19,12,19z"
-                strokeLinejoin="round"
-            />
-        </g>
+    <Icon viewBox="0 0 24 24" stroke="currentColor" {...props}>
+        <path
+            d="M5.33341 9.33333C3.86008 9.33333 2.66675 8.14 2.66675 6.66667C2.66675 5.19333 3.86008 4 5.33341 4C6.80675 4 8.00008 5.19333 8.00008 6.66667C8.00008 8.14 6.80675 9.33333 5.33341 9.33333Z"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+        <path
+            d="M8.00004 28H4.00004V22.6667H1.33337V16C1.33337 14.5267 2.52671 13.3334 4.00004 13.3334H5.33337"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+        <path
+            d="M26.6667 9.33333C28.14 9.33333 29.3333 8.14 29.3333 6.66667C29.3333 5.19333 28.14 4 26.6667 4C25.1933 4 24 5.19333 24 6.66667C24 8.14 25.1933 9.33333 26.6667 9.33333Z"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+        <path
+            d="M24 28H28V22.6667H30.6667V16C30.6667 14.5267 29.4733 13.3334 28 13.3334H26.6667"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+        <path
+            d="M16 9.33337C13.7907 9.33337 12 7.54271 12 5.33337C12 3.12404 13.7907 1.33337 16 1.33337C18.2093 1.33337 20 3.12404 20 5.33337C20 7.54271 18.2093 9.33337 16 9.33337Z"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+        <path
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M20 30.6667H12V22.6667H9.33337V16C9.33337 14.5267 10.5267 13.3334 12 13.3334H20C21.4734 13.3334 22.6667 14.5267 22.6667 16V24H20V30.6667Z"
+        />
     </Icon>
 );
 
