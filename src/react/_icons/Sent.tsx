899d75e9556c874b2defb52120cbe9cfa4693182
sara-tustra <77685910+sara-tustra@users.noreply.github.com>
2022-06-15 16:57:49 -0400

    feat: sent and shareArrow icons

---
+++ src/react/_icons/Sent.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Icon from '../components/Icon';
+import { IconComponent } from '../types';
+
+const Sent: IconComponent = (props) => (
+    <Icon viewBox="0 0 100 101" fill="none" {...props}>
+        <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeMiterlimit="10"
+            strokeWidth="1.5"
+            d="M28.678 95.936L41.516 83.29M22.584 77.963l12.804-12.61M4.166 71.793l12.838-12.645M45.385 55L62.88 85.349c.197.338.488.614.839.796a2.115 2.115 0 002.23-.18 2.06 2.06 0 00.696-.92L95.711 6.85a2.027 2.027 0 00-.49-2.148 2.119 2.119 0 00-2.181-.482L12.143 33.353a2.086 2.086 0 00-.95.694 2.039 2.039 0 00-.163 2.229c.193.347.485.632.84.82l32.694 17.096c.345.189.63.47.821.809v0zM44.975 54.63l50.12-49.905"
+        ></path>
+    </Icon>
+);
+
+export default Sent;
