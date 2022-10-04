b1a9c16f648152e71691f960066b1ea122674dd2
Miguel Cádiz <98823824+mcadizwtw@users.noreply.github.com>
2022-10-04 11:42:53 -0300

    `PTS-527` Cruise icon (#72)

---
+++ src/react/_icons/Cruise.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Icon from '../components/Icon';
+import { IconComponent } from '../types';
+
+const Cruise: IconComponent = (props) => (
+    <Icon viewBox="0 0 24 22" fill="transparent" {...props}>
+        <path
+            d="M20 20L22.6148 13.0272C22.8215 12.4759 22.5096 11.8663 21.9416 11.7113L12.2631 9.07176C12.0909 9.02478 11.9091 9.02478 11.7369 9.07176L2.05842 11.7113C1.4904 11.8663 1.17848 12.4759 1.38521 13.0272L4 20"
+            stroke="currentColor"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+        <path
+            d="M6.5 14L11.7369 12.5718C11.9091 12.5248 12.0909 12.5248 12.2631 12.5718L17.5 14"
+            stroke="currentColor"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+        <path
+            d="M4 11.5V6C4 5.44772 4.44772 5 5 5H19C19.5523 5 20 5.44772 20 6V11.5"
+            stroke="currentColor"
+            stroke-width="1.5"
+        />
+        <path
+            d="M9 5V2C9 1.44772 9.44772 1 10 1H14C14.5523 1 15 1.44772 15 2V5"
+            stroke="currentColor"
+            stroke-width="1.5"
+        />
+        <path
+            d="M1 21C3.86957 21 2.91304 20 4.82609 20C7.21739 20 6.26087 21 8.65217 21C10.5652 21 10.5652 20 12 20C14.3913 20 14.3913 21 15.8261 21C17.7391 21 17.2609 20 19.1739 20C21.087 20 21.5652 21 23 21"
+            stroke="currentColor"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+    </Icon>
+);
+
+export default Cruise;
