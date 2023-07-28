d030be538f9cf0bce7ac48f5960926b6285ca720
Pablo Montesano <pmontesanodm@gmail.com>
2023-07-28 17:38:55 -0300

    change stroke color

--- src/react/_icons/OnSale.tsx
+++ src/react/_icons/OnSale.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import Icon from '../components/Icon';
 import { IconComponent } from '../types';
 
-const OnSale: IconComponent = (props) => (
+const OnSale: IconComponent = ({ fill = 'currentColor', stroke = 'currentColor', ...props }) => (
     <Icon viewBox="0 0 26 24" {...props} fill="none">
         <g id="Group 27">
             <g id="Group 26">
                 <path
                     id="Vector"
                     d="M15.9147 8.07373L11.4771 15.7599"
-                    stroke="#2353AB"
+                    stroke={stroke}
                     stroke-miterlimit="10"
                     stroke-linecap="round"
                     stroke-linejoin="round"
@@ -17,7 +17,7 @@ const OnSale: IconComponent = (props) => (
                 <path
                     id="Vector_2"
                     d="M16.9644 10.3047C16.4046 9.32926 13.9622 7.43238 12.9894 9.11729C11.8249 11.1343 15.6052 12.3675 14.5775 14.1474C13.5499 15.9272 11.4376 14.5291 10.2551 13.0756"
-                    stroke="#2353AB"
+                    stroke={stroke}
                     stroke-miterlimit="10"
                     stroke-linecap="round"
                     stroke-linejoin="round"
@@ -26,7 +26,7 @@ const OnSale: IconComponent = (props) => (
             <path
                 id="Rectangle 77"
                 d="M10.9507 3.08102L11.1616 3.80075L10.9507 3.08102L3.65409 5.21916C3.01327 5.40694 2.63321 6.06523 2.791 6.71408L4.58763 14.1022L5.31639 13.925L4.58763 14.1022C4.66835 14.4342 4.88139 14.7186 5.17723 14.8894L17.7855 22.1688C18.3834 22.514 19.1479 22.3091 19.493 21.7112L24.993 12.185C25.3382 11.5871 25.1334 10.8226 24.5355 10.4774L11.9272 3.19805C11.6314 3.02724 11.2785 2.98496 10.9507 3.08102Z"
-                stroke="#2353AB"
+                stroke={stroke}
                 stroke-width="1.5"
             />
             <ellipse
@@ -36,7 +36,7 @@ const OnSale: IconComponent = (props) => (
                 rx="0.7"
                 ry="0.7396"
                 transform="rotate(30 6.47946 7.75101)"
-                fill="#2353AB"
+                stroke={stroke}
             />
         </g>
     </Icon>
