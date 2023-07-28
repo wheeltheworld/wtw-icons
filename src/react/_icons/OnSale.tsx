6edbd39ad7255c910e1e9cfe355de39ff59d73af
Pablo Montesano <pmontesanodm@gmail.com>
2023-07-28 15:33:47 -0300

    Feature/pts 1563 onsale icon (#83)
    * new onsale icon
    
    * change version

---
+++ src/react/_icons/OnSale.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Icon from '../components/Icon';
+import { IconComponent } from '../types';
+
+const OnSale: IconComponent = (props) => (
+    <Icon viewBox="0 0 24 24" {...props}>
+        <g id="Group 27">
+            <g id="Group 26">
+                <path
+                    id="Vector"
+                    d="M11.2764 5.38232L8.31798 10.5064"
+                    stroke="#2353AB"
+                    stroke-width="0.75"
+                    stroke-miterlimit="10"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                />
+                <path
+                    id="Vector_2"
+                    d="M11.9762 6.86966C11.603 6.21935 9.97472 4.95476 9.32619 6.07803C8.54983 7.42273 11.07 8.24482 10.3849 9.43141C9.69986 10.618 8.29165 9.6859 7.50335 8.7169"
+                    stroke="#2353AB"
+                    stroke-width="0.75"
+                    stroke-miterlimit="10"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                />
+            </g>
+            <path
+                id="Rectangle 77"
+                d="M8.67943 2.16753C8.44276 2.03088 8.16048 1.99706 7.89822 2.0739L8.03883 2.55373L7.89822 2.0739L3.25491 3.43454C2.74226 3.58476 2.43821 4.11139 2.56443 4.63048L3.70775 9.33202C3.77232 9.59757 3.94276 9.82511 4.17943 9.96175L12.3788 14.6957C12.8571 14.9718 13.4687 14.808 13.7449 14.3297L17.2449 8.26749C17.521 7.78919 17.3571 7.1776 16.8788 6.90146L8.67943 2.16753Z"
+                stroke="#2353AB"
+            />
+            <ellipse
+                id="Ellipse 1"
+                cx="4.98574"
+                cy="5.16718"
+                rx="0.466667"
+                ry="0.493066"
+                transform="rotate(30 4.98574 5.16718)"
+                fill="#2353AB"
+            />
+        </g>
+    </Icon>
+);
+
+export default OnSale;
