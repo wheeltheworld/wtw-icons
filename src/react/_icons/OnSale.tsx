17572f0373a0536311603e41f87444810816d5a6
Pablo Montesano <pmontesanodm@gmail.com>
2023-07-28 15:56:21 -0300

    modify onSale icon

--- src/react/_icons/OnSale.tsx
+++ src/react/_icons/OnSale.tsx
@@ -4,42 +4,14 @@ import { IconComponent } from '../types';
 
 const OnSale: IconComponent = (props) => (
     <Icon viewBox="0 0 24 24" {...props}>
-        <g id="Group 27">
-            <g id="Group 26">
-                <path
-                    id="Vector"
-                    d="M11.2764 5.38232L8.31798 10.5064"
-                    stroke="#2353AB"
-                    stroke-width="0.75"
-                    stroke-miterlimit="10"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                />
-                <path
-                    id="Vector_2"
-                    d="M11.9762 6.86966C11.603 6.21935 9.97472 4.95476 9.32619 6.07803C8.54983 7.42273 11.07 8.24482 10.3849 9.43141C9.69986 10.618 8.29165 9.6859 7.50335 8.7169"
-                    stroke="#2353AB"
-                    stroke-width="0.75"
-                    stroke-miterlimit="10"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                />
-            </g>
-            <path
-                id="Rectangle 77"
-                d="M8.67943 2.16753C8.44276 2.03088 8.16048 1.99706 7.89822 2.0739L8.03883 2.55373L7.89822 2.0739L3.25491 3.43454C2.74226 3.58476 2.43821 4.11139 2.56443 4.63048L3.70775 9.33202C3.77232 9.59757 3.94276 9.82511 4.17943 9.96175L12.3788 14.6957C12.8571 14.9718 13.4687 14.808 13.7449 14.3297L17.2449 8.26749C17.521 7.78919 17.3571 7.1776 16.8788 6.90146L8.67943 2.16753Z"
-                stroke="#2353AB"
-            />
-            <ellipse
-                id="Ellipse 1"
-                cx="4.98574"
-                cy="5.16718"
-                rx="0.466667"
-                ry="0.493066"
-                transform="rotate(30 4.98574 5.16718)"
-                fill="#2353AB"
-            />
+        <g stroke="#2353AB" stroke-width=".75" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round">
+            <path d="m11.276 5.382-2.958 5.124M11.976 6.87c-.373-.65-2.001-1.915-2.65-.792-.776 1.345 1.744 2.167 1.059 3.353-.685 1.187-2.093.255-2.882-.714" />
         </g>
+        <path
+            d="M8.68 2.168a1 1 0 0 0-.782-.094l.14.48-.14-.48-4.643 1.36a1 1 0 0 0-.69 1.196l1.143 4.702a1 1 0 0 0 .471.63l8.2 4.734a1 1 0 0 0 1.366-.366l3.5-6.063a1 1 0 0 0-.366-1.366l-8.2-4.733z"
+            stroke="#2353AB"
+        />
+        <ellipse cx="4.986" cy="5.167" rx=".467" ry=".493" transform="rotate(30 4.986 5.167)" fill="#2353AB" />
     </Icon>
 );
 
