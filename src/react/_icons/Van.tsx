fac7483be302b99ef0d1000f3676a5eb38647306
Leiser Mahu <leiser.mahu@gmail.com>
2024-02-08 15:41:12 -0300

    `Fix/MATE-2210` Reimplement van icon (#90)
    * fix: reimplement icon
    
    * chore: app
    
    * 2.8.1

--- src/react/_icons/Van.tsx
+++ src/react/_icons/Van.tsx
@@ -3,24 +3,23 @@ import Icon from '../components/Icon';
 import { IconComponent } from '../types';
 
 const Van: IconComponent = (props) => (
-    <Icon viewBox="0 0 24 24" {...props}>
-       <g
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeMiterlimit="10"
-        strokeWidth="1.5"
-        clipPath="url(#clip0)"
-        fill="none"
-      >
-        <path d="M21.8 20.7c.8-.1 1.3-.8 1.3-1.6s0-1.3-.4-1.9c0-.1-.1-.1-.1-.1-.7-.9-1.8-1.5-3-1.5h-.5l-3.4-3.3c-.7-.6-1.5-1-2.4-1H2.7c-1 0-1.8.8-1.8 1.8v5.8c.2.9 1 1.7 2 1.7M7.5 20.7h9.7M18.8 15.6H1.3M10.7 11.4v3.9M15.5 18.2H9.3"></path>
-        <path d="M5.2 23.4a2.2 2.2 0 100-4.4 2.2 2.2 0 000 4.4zM19.3 23.4a2.2 2.2 0 100-4.4 2.2 2.2 0 000 4.4z"></path>
-      </g>
-      <defs>
-        <clipPath id="clip0">
-          <path fill="currentColor" d="M0 0H24V24H0z"></path>
-        </clipPath>
-      </defs>
+    <Icon viewBox="0 0 24 24" {...props} fill="none">
+        <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeMiterlimit="10"
+            strokeWidth="1.5"
+            d="M21.712 15.315c.793-.099 1.288-.792 1.288-1.585 0-.793 0-1.289-.396-1.883 0-.1-.1-.1-.1-.1-.693-.891-1.783-1.486-2.973-1.486h-.495l-3.37-3.27C14.974 6.396 14.18 6 13.289 6H2.784C1.793 6 1 6.793 1 7.784v5.748c.198.891.991 1.684 1.982 1.684M7.541 15.315h9.613M18.739 10.261H1.396M10.712 6.1v3.864M15.468 12.838H9.324"
+        ></path>
+        <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeMiterlimit="10"
+            strokeWidth="1.5"
+            d="M5.261 17.99a2.18 2.18 0 100-4.36 2.18 2.18 0 000 4.36zM19.235 17.99a2.18 2.18 0 100-4.36 2.18 2.18 0 000 4.36z"
+        ></path>
     </Icon>
 );
 
