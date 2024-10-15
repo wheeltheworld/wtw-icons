d7a1c5fc4e479031722ef989b6a0c916d62d13a4
Leiser Mahu <leiser.mahu@gmail.com>
2024-10-15 15:34:19 -0300

    `Chore/MATE-3129` Update icons part 1/4 (#100)
    * chore: updating icons
    
    * chore: website
    
    * chore: updating arrow icons to be bigger
    
    * chore: fix arrows
    
    * chore: build website
    
    * chore: update to current version
    
    * 2.18.0
    
    * 2.19.0
    
    * 2.20.0
    
    * chore: going back to 17.1
    
    * 2.18.0
    
    * chore: update app

--- src/react/_icons/ArrowBack.tsx
+++ src/react/_icons/ArrowBack.tsx
@@ -3,13 +3,12 @@ import Icon from '../components/Icon';
 import { IconComponent } from '../types';
 
 const ArrowBack: IconComponent = (props) => (
-    <Icon viewBox="0 0 10 17" fill="none" {...props}>
+    <Icon viewBox="0 0 24 24" fill="none" {...props}>
         <path
-            d="M9.03894 1L0.999941 8.663L9.03894 16"
-            stroke="currentColor"
-            strokeWidth={1.5}
-            strokeLinecap="round"
-            strokeLinejoin="round"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M16.0438 4.61955C16.4343 5.01008 16.4343 5.64324 16.0438 6.03377L10.0775 12L16.0438 17.9662C16.4343 18.3568 16.4343 18.9899 16.0438 19.3804C15.6533 19.771 15.0201 19.771 14.6296 19.3804L7.95622 12.7071C7.5657 12.3166 7.5657 11.6834 7.95622 11.2929L14.6296 4.61955C15.0201 4.22903 15.6533 4.22903 16.0438 4.61955Z"
+            fill="currentColor"
         />
     </Icon>
 );
