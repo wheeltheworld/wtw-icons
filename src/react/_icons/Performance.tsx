9c371518917bce638def4785d76ecfeffd567301
Joan Rotbard <joan@wheeltheworld.com>
2024-12-17 18:29:10 +0400

    performance icon

---
+++ src/react/_icons/Performance.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Icon from '../components/Icon';
+import { IconComponent } from '../types';
+
+const Performance: IconComponent = (props) => (
+    <Icon viewBox="0 0 24 24" {...props} fill="none">
+        <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1.29956 1.30542L1.29956 14.0615L14.318 14.0617M4.61976 10.3447L6.92511 7.05361C7.22775 6.62157 7.84708 6.56744 8.22008 6.94044C8.62018 7.34053 9.29236 7.24437 9.56423 6.74815L11.8198 3.38915" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+    </Icon>
+);
+
+export default Performance;
