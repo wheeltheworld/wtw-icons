9f18ee668d8d7290182ab7a26d136030eaffa96f
Tisks <leiser.mahu@usach.cl>
2025-11-19 16:55:25 -0300

    feat: icons

---
+++ src/react/_icons/FloatingGraduationHat.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import Icon from '../components/Icon';
+import { IconComponent } from '../types';
+
+const FloatingGraduationHat: IconComponent = (props) => (
+    <Icon viewBox="0 0 24 24" {...props}>
+        <svg width="24" height="24" viewBox="0 0 40 42" xmlns="http://www.w3.org/2000/svg" fill="none">
+            <path fill="#fff" d="m20 7.1 16 7.1-16 7.7-16.2-7.4z"></path>
+            <path
+                fill="url(#paint0_linear_1600_104)"
+                d="m33.8 22.4-1 1.1-.4 6.3.4 1.5 2.2.1.6-1.2-.7-3.1-.3-3.1z"
+            ></path>
+            <path
+                fill="url(#paint1_linear_1600_104)"
+                d="M9.9 18.1v7.8l4 2.3 8.6.7 6.6-1.8 1.6-3.5-.4-6.1-8.7 3.1z"
+            ></path>
+            <path
+                fill="url(#paint2_linear_1600_104)"
+                d="m21 7.9 9.4 5.8c.4.2.4.8 0 1l-10.7 6.8q-.3.15-.6 0l-13.6-7c-.5-.2-.4-.9 0-1.1l14.9-5.6h.5z"
+            ></path>
+            <path
+                fill="currentColor"
+                d="M36.8 13.2 20.6 6.1c-.3-.1-.7-.1-1 0L3.3 13.3c-.5.2-.7.6-.7 1.1s.3.9.7 1.1l5.2 2.4v7.5c.1.5 1.2 4.8 11.5 4.8s11.2-4.3 11.3-4.5v-.2c.1 0 .1-7.5.1-7.5l1.6-.7v4.8c-.7.3-1.2 1-1.2 1.8s.3 1.1.7 1.5c-.5 1.2-1.2 3-1.2 4 0 1.7 1.1 3 2.5 3s2.4-1.3 2.4-3-.6-2.7-1.1-4c.4-.4.7-.9.7-1.5s-.5-1.5-1.2-1.8v-5.5l2.3-1c.4-.2.7-.6.7-1.1s-.3-.9-.8-1.1zm-7.4 11.4c0 .3 0 .5-.3.7-.9.9-3.3 2.9-9.1 2.9s-8.8-2-9.3-2.8-.1-.3-.1-.5v-6.1l9 4.1c.2 0 .3.1.5.1s.3 0 .5-.1l8.8-4zm-9.8-3.9-13.3-6c-.2-.1-.2-.4 0-.6l13.4-5.8c.3-.1.6-.1.8 0l13.3 5.8c.2.1.2.4 0 .6l-13.3 6c-.3.1-.6.1-.8 0zm14.7 3.1c0 .4-.2.7-.5.7s-.5-.3-.5-.7.2-.7.5-.7.5.3.5.7m.4 5.6c0 .7-.3 1.5-.9 1.5s-1-.7-1-1.5.5-2 1-3.2c.5 1.2.9 2.6.9 3.2M19.7 35.9c6.793 0 12.3-.672 12.3-1.5s-5.507-1.5-12.3-1.5-12.3.672-12.3 1.5 5.507 1.5 12.3 1.5"
+            ></path>
+
+            <defs>
+                <linearGradient
+                    id="paint0_linear_1600_104"
+                    x1="30.5"
+                    x2="36.3"
+                    y1="23.1"
+                    y2="30.9"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop stop-color="#fff"></stop>
+                    <stop offset="1" stop-color="#E0F4F5"></stop>
+                </linearGradient>
+
+                <linearGradient
+                    id="paint1_linear_1600_104"
+                    x1="10.5"
+                    x2="28.6"
+                    y1="17.3"
+                    y2="26"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop stop-color="#fff"></stop>
+                    <stop offset="1" stop-color="#E0F4F5"></stop>
+                </linearGradient>
+
+                <linearGradient
+                    id="paint2_linear_1600_104"
+                    x1="6.756"
+                    x2="29.049"
+                    y1="17.081"
+                    y2="13.149"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop stop-color="#fff"></stop>
+                    <stop offset="1" stop-color="#E0F4F5"></stop>
+                </linearGradient>
+            </defs>
+        </svg>
+    </Icon>
+);
+
+export default FloatingGraduationHat;
